62be66d903c6bf03b96168916bcabcf4217b7313
Egemen Berker Kızılcan <egemen@umomega.com>
2020-12-05 18:56:03 +0300

    Added Turkish translations

---
+++ resources/lang/tr/tags.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+	'manage' => 'Etiketleri Yönet',
+
+	'all' => 'Tüm Etiketler',
+	'multiple' => 'Etiketler',
+	'single' => 'Etiket',
+	'create' => 'Etiket Oluştur',
+	'created'=> 'Etiket oluşturuldu.',
+	'edit' => 'Etiketi Düzenle',
+	'edited' => 'Etiket düzenlendi.',
+	'delete' => 'Etiketi Sil',
+	'deleted' => 'Etiket silindi.',
+	'deleted_multiple' => 'Etiketler silindi.',
+	'translate' => 'Etikete Çeviri Ekle',
+	'translated' => 'Etikete çeviri eklendi.',
+
+	'event_stored' => 'yeni bir etiket oluşturdu',
+	'event_updated' => 'bir etiketi güncelledi',
+	'event_destroyed' => 'bir etiketi sildi',
+	'event_destroyed_bulk' => 'etiketleri sildi',
+	'event_translated' => 'bir etikete çeviri ekledi',
+	'event_translation_destroyed' => 'bir etiketin çevirisini sildi',
+];
